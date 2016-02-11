f63966d08835d9c9b2a64a46cf0edcc26d902561
perdorues <perdorues@user-PC>
2016-02-11 12:25:27 +0100

    from leart

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-		System.out.println("Hello World from leart");
+		System.out.println("Hello World , its me, leart");
 
 	}
 
